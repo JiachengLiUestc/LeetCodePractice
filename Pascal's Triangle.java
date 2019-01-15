f82de7d696c52d744f2ee3fcca9c0141485ffd0c
JiachengLiUestc <46712912+JiachengLiUestc@users.noreply.github.com>
2019-01-15 20:33:42 +0800

    Update Pascal's Triangle.java

--- Pascal's Triangle.java
+++ Pascal's Triangle.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+         List<List<Integer>> list=new ArrayList<List<Integer>>();
+        int[][] array=new int[numRows][numRows];
+        for(int i=0;i<numRows;i++){
+            List<Integer> temp = new ArrayList<Integer>();
+            for(int j=0;j<i+1;j++){
+                if(j==0||j==i){
+                    array[i][j] = 1;
+                    temp.add(1);
+                }else{
+                    array[i][j] = array[i-1][j-1]+array[i-1][j];
+                    temp.add(array[i][j]);
+                }
+            }
+            list.add(temp);
+        }
+        return list;
+    }
+}
